e3f2be9f16098ea077ca12071e20d9bd5256ca34
Raider7019 <warrior80118@gmail.com>
2022-11-24 17:07:50 +0400

    Add legacy mode support

--- modules/common-defs.php
+++ modules/common-defs.php
@@ -3,4 +3,5 @@
     define("TSURI", "/cosmos/bank/v1beta1/supply/");
     define("STURI", "/cosmos/staking/v1beta1/pool");
     define("CPURI", "/cosmos/distribution/v1beta1/community_pool");
+    define("CSURI", "/v1/circulatingsupply/");
 ?>
--- modules/csupply.php
+++ modules/csupply.php
@@ -10,41 +10,57 @@
         switch($denom)
         {
             case 'lunc':
-                // Get the current total supply
-                $tsjson = file_get_contents($config->lcd . TSURI . "uluna");
-                $totalSupply = json_decode($tsjson, false)->amount->amount;
-
-                // Retrieve staking data
-                $stjson = file_get_contents($config->lcd . STURI);
-                $bondedTokens = json_decode($stjson, false)->pool->bonded_tokens;
-
-                // Get the community pool uluna amount
-                $communityPool = getCpool($config, "uluna");
-
-                if ($config->debug)
+                if (!$config->legacy
                 {
-                    echo "DEBUG: ULUNA TS: $totalSupply BT: $bondedTokens CP: $communityPool<br>";
-                }   
+                    // Get the current total supply
+                    $tsjson = file_get_contents($config->lcd . TSURI . "uluna");
+                    $totalSupply = json_decode($tsjson, false)->amount->amount;
+
+                    // Retrieve staking data
+                    $stjson = file_get_contents($config->lcd . STURI);
+                    $bondedTokens = json_decode($stjson, false)->pool->bonded_tokens;
+
+                    // Get the community pool uluna amount
+                    $communityPool = getCpool($config, "uluna");
+
+                    if ($config->debug)
+                    {
+                        echo "DEBUG: ULUNA TS: $totalSupply BT: $bondedTokens CP: $communityPool<br>";
+                    }   
         
-                // Calculate the revised circulating supply in luna
-                $cSupply = bcdiv(bcsub(bcsub($totalSupply, $bondedTokens), $communityPool), '1000000', 6);
+                    // Calculate the revised circulating supply in luna
+                    $cSupply = bcdiv(bcsub(bcsub($totalSupply, $bondedTokens), $communityPool), '1000000', 6);
+                }
+                else
+                {
+                    // Return the legacy FCD circulating supply figure
+                    cSupply = file_get_contents($config->fcd . CSURI . "lunc");
+                }
                 break;
 
             case 'ustc':
-                // Get the current total supply
-                $tsjson = file_get_contents($config->lcd . TSURI . "uusd");
-                $totalSupply = json_decode($tsjson, false)->amount->amount;
-
-                // Get the community pool uluna amount
-                $communityPool = getCpool($config, "uusd");
-
-                if ($config->debug)
+                if (!$config->legacy)
                 {
-                    echo "DEBUG: UUSD TS: $totalSupply CP: $communityPool<br>";
-                }   
+                    // Get the current total supply
+                    $tsjson = file_get_contents($config->lcd . TSURI . "uusd");
+                    $totalSupply = json_decode($tsjson, false)->amount->amount;
+
+                    // Get the community pool uluna amount
+                    $communityPool = getCpool($config, "uusd");
+
+                    if ($config->debug)
+                    {
+                        echo "DEBUG: UUSD TS: $totalSupply CP: $communityPool<br>";
+                    }   
         
-                // Calculate the revised circulating supply in ustc
-                $cSupply = bcdiv(bcsub($totalSupply, $communityPool), '1000000', 6);
+                    // Calculate the revised circulating supply in ustc
+                    $cSupply = bcdiv(bcsub($totalSupply, $communityPool), '1000000', 6);
+                }
+                else
+                {
+                    // Return the legacy FCD circulating supply figure
+                    cSupply = file_get_contents($config->fcd . CSURI . "ust");
+                }
                 break;
 
             default:
